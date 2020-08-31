94ab29f78776b7b993ca7e4cca86eae99ae24641
rsagrawal2996 <rsagrawal.avanti@gmail.com>
2020-08-31 10:33:21 +0530

    eureka server added

---
+++ eureka-server/src/main/java/com/example/demo/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+//import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+
+//@EnableAdminServer
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
